a9682d3c44b44f255e912474bba30aca0b13a17b
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-10-19 03:19:15 +0000

    login with fetch

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ]  = useState("");
+  const token = sessionStorage.getItem("token");
+
+  const handleClick = () => {
+    actions.login(email, password).then(() => {
+      
+    })
+  }
+
+/* Crea el form para que user ingrese los datos y se asegura de que user se haya logueado solo 1 vez. */
+  return (
+    <div className="text-center mt-5">
+      <h1>Login Page</h1>
+      {token && token != "" && token != undefined ? ("Se ha logueado con el token: " + token) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClick}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,85 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      login: async (email, password) => {
+        /*  Metodo para enviar el email y password ingresados por el usuario */
+        const results = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+        /* Trae la informacion del backend por medio del fetch */
+        try {
+          const resp = await fetch(
+            "https://3001-jengm-sistemadeautenti-05qbzxxbgzn.ws-us71.gitpod.io/api/token",
+            results
+          );
+          if (resp !== 200) {
+            alert("Hay un error en el fetch");
+            return false;
+          }
+          const data = await resp.json();
+          console.log("Result from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+        } catch (error) {
+          console.error("Error al cargar", error);
+        }
+      },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
